e81aa3f9cb4da1da40d010226364fe0f5d6d7cd0
Sebastian Sibelle <sebastian.sibelle@squareweave.com.au>
2014-09-02 12:55:09 +1000

    Added a failing unit test for move

--- spec/lib/robot_spec.rb
+++ spec/lib/robot_spec.rb
@@ -13,4 +13,18 @@ describe Robot do
       expect(robot.orientation).to eq(Orientation::NORTH)
     end
   end
+
+  describe "move" do
+    it "moves the robot's location" do
+      table = Table.new(5,5)
+      robot = Robot.new(table)
+      position = Position.new(2,3)
+      orientation = Orientation::NORTH
+      robot.place(position, orientation)
+      robot.move 
+
+      expect(robot.position).to eq(Position(2,4))
+      expect(robot.orientation).to eq(Orientation::NORTH)
+    end
+  end
 end
