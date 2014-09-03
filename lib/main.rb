c947c3cce78a079516e18bdccb43abd98063ddab
Sebastian Sibelle <sebastian.sibelle@squareweave.com.au>
2014-09-03 13:33:25 +1000

    Added introduction text

--- lib/main.rb
+++ lib/main.rb
@@ -7,5 +7,11 @@ if ARGV.length > 0
   exit 1
 end
 
+STDERR.puts "Welcome to the Toy Robot Simulator!"
+STDERR.puts "We are simulating a toy robot moving on a square tabletop."
+STDERR.puts "The tabletop is #{TABLE_X} unit wide by #{TABLE_Y} units high."
+STDERR.puts "Valid commands include: PLACE X,Y,F | MOVE | LEFT | RIGHT | REPORT."
+STDERR.puts "The first valid command must be a PLACE X,Y,F command"
+
 application = Application.new(STDIN, STDOUT)
 application.run
