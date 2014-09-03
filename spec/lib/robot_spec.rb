2545f33841b6a4c1a9be13c83d1d68b3ca37889c
Sebastian Sibelle <sebastian.sibelle@squareweave.com.au>
2014-09-03 16:08:49 +1000

    Added the updated robot left and right unit test

--- spec/lib/robot_spec.rb
+++ spec/lib/robot_spec.rb
@@ -44,4 +44,36 @@ describe Robot do
       end
     end
   end
+
+  describe "left" do
+    it "rotates the robot 90 degrees to the left" do
+      table = Table.new(5, 5)
+      robot = Robot.new(table)
+      position = Position.new(1,1)
+      orientation = Orientation::NORTH
+      placement = Placement.new(table, position, orientation)
+      robot.place(placement)
+
+      robot.left
+
+      expect(robot.placement.position).to eq(Position.new(1,1))
+      expect(robot.placement.orientation).to eq(Orientation::WEST)
+    end
+  end
+
+  describe "right" do
+    it "rotates the robot 90 degrees to the right" do
+      table = Table.new(5, 5)
+      robot = Robot.new(table)
+      position = Position.new(1,1)
+      orientation = Orientation::NORTH
+      placement = Placement.new(table, position, orientation)
+      robot.place(placement)
+
+      robot.right
+
+      expect(robot.placement.position).to eq(Position.new(1,1))
+      expect(robot.placement.orientation).to eq(Orientation::EAST)
+    end
+  end
 end
