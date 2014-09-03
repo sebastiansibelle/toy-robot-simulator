4db4709e2258367fcf9a4a7de56073d45471bb87
Sebastian Sibelle <sebastian.sibelle@squareweave.com.au>
2014-09-03 13:37:29 +1000

    Removed STDIN check

--- lib/main.rb
+++ lib/main.rb
@@ -2,11 +2,6 @@ require_relative 'robot'
 require_relative 'table'
 require_relative 'application'
 
-if ARGV.length > 0
-  puts "please use standard input for this program (or give it option parsing thing)"
-  exit 1
-end
-
 STDERR.puts "Welcome to the Toy Robot Simulator!"
 STDERR.puts "We are simulating a toy robot moving on a square tabletop."
 STDERR.puts "The tabletop is #{TABLE_X} unit wide by #{TABLE_Y} units high."
