7a9d6a99eee5cc75ac17fb1839ff891f42d9c64c
Sebastian Sibelle <sebastian.sibelle@squareweave.com.au>
2014-09-03 11:40:14 +1000

    Added basic implementation of parse

--- lib/application.rb
+++ lib/application.rb
@@ -2,13 +2,35 @@
 # Main application class runs from here.
 # 
 class Application
+
+  TABLE_X = 5
+  TABLE_Y = 5
+
+  InvalidCommand = Class.new(Exception)
+
   def initialize(stdin, stdout)
-    @table = Table.new(5,5)
+    @table = Table.new(TABLE_X, TABLE_Y)
     @robot  = Robot.new(@table)
     @stdin  = stdin
     @stdout = stdout
   end
 
-  def run
+  def self.parse(token)
+    command, arguments = token.split(" ")
+    arguments = String(arguments).split(",")
+
+    case command
+    when 'PLACE'
+      x,y,orientation = arguments
+      type = :place
+      { type: type, :arguments => [x, y, orientation ] }
+      
+    when 'LEFT','RIGHT','MOVE','REPORT'
+      type = command.downcase.to_sym
+      { type: type }
+      
+    else
+      raise InvalidCommand.new("#{command} is an invalid command")
+    end
   end
 end
