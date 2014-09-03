dec791ee6e313ceef8753d5b6861f29d8ea4229b
Sebastian Sibelle <sebastian.sibelle@squareweave.com.au>
2014-09-03 16:11:58 +1000

    Added a unit test for position .to_s

--- spec/lib/position_spec.rb
+++ spec/lib/position_spec.rb
@@ -1,4 +1,12 @@
 require "position"
 
 describe Position do
+  describe "to_s" do
+    it "converts a position to a string" do
+      position = Position.new(2,3)
+      string = position.to_s 
+
+      expect(string).to eq("2,3")
+    end
+  end
 end
